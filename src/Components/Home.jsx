e55fc432a6695bed0a51675bc62c8d6a04f88e27
Himesh-Kukadiya <himeshkukadiya075@gmail.com>
2024-07-31 00:41:15 +0530

    Complete Hero Section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,16 @@
+import {home} from '../Script/assets.js';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <header id="home" className="header">
+            <img src={home} alt="Dark Background Image" className="header-image" />
+                <div className="overlay"></div>
+                <div className="header-content">
+                    <h1 className="display-4 text-light animated fadeInDown">Restaurant Equipment Rental</h1>
+                    <p className="lead text-light animated fadeInUp">High-quality equipment for your restaurant needs</p>
+                    <a href="#services" className="btn btn-primary btn-lg animated fadeIn">Learn More</a>
+                </div>
+        </header>
     )
 }
 
---
+++ src/Script/assets.js
@@ -0,0 +1,28 @@
+// home page
+import home from '../assets/Home/home1.jpg';
+// about page
+import about from '../assets/About/about1.jpg';
+// product list page
+import product1 from '../assets/Product/product1.jpg'
+import product2 from '../assets/Product/product2.jpeg'
+import product3 from '../assets/Product/product3.jpeg'
+import product4 from '../assets/Product/product4.webp'
+import product5 from '../assets/Product/product5.jpg'
+import product6 from '../assets/Product/product6.webp'
+// galary page
+import galary1 from '../assets/Galary/product1.jpg'
+import galary2 from '../assets/Galary/product2.jpeg'
+import galary3 from '../assets/Galary/product3.jpeg'
+import galary4 from '../assets/Galary/product4.webp'
+import galary5 from '../assets/Galary/product5.jpg'
+import galary6 from '../assets/Galary/product6.webp'
+// contact page
+import contact from '../assets/Contact/contact1.png'
+
+export {
+    home,
+    about,
+    product1, product2, product3, product4, product5, product6,
+    galary1, galary2, galary3, galary4, galary5, galary6,
+    contact
+};
