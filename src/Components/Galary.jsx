3802a050512d7ef65dfea15e33db85f038f0eccf
Himesh-Kukadiya <himeshkukadiya075@gmail.com>
2024-07-31 11:08:49 +0530

    Complete Gallery Section

--- src/Components/Galary.jsx
+++ src/Components/Galary.jsx
@@ -1,7 +1,23 @@
+import { galleryList } from "../Script/index";
 
 const Galary = () => {
     return (
-        <div>Galary</div>
+        <section id="gallery" className="gallery-section">
+            <div className="container">
+                <h2 className="section-title text-center">Our Gallery</h2>
+                <div className="row">
+                    {
+                        galleryList.map(gallery => (
+                            <div className="col-lg-4 col-md-6" key={gallery.id} >
+                                <div className="gallery-item">
+                                    <img src={gallery.image} alt="Gallery Image 1" className="img-fluid" />
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
     )
 }
 
